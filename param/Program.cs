b501cb84b1ef20f6642bd2f3344f81d6cf5498d9
Winston <wrodriguez@redsis.com>
2020-02-04 08:32:54 -0500

    Proyecto que recibe argumento por consola

---
+++ param/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace param
+{
+    class Program
+    {
+        static Dictionary<string, string> paramList = new Dictionary<string, string>();
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                string key = args[i].ToLower(); ;
+                if (key.Equals("-h"))
+                {
+                    MostrarAyuda();
+                }else if(!key.Equals("-n") && !key.Equals("-h") && !key.Equals("-a"))
+                {
+                    MostrarOpciones(key);
+                    return;
+                }
+                else
+                {
+                    i++;
+                    string value = args[i];
+                    paramList.Add(key, value);
+                }   
+                           
+            }
+
+            
+            //string valorH = paramList["-n"];
+            //string valorT = paramList["-a"];
+            //Console.WriteLine("Nombre: {0}", valorH);
+            //Console.WriteLine("Apellido: {0}", valorT);
+            Console.ReadLine();
+        }
+
+        private static void MostrarOpciones(string key)
+        {
+            Console.WriteLine("Opcion desconocida {0}", key);
+            Console.WriteLine("Uso: param [-h] [-n <value>] [-a <value>]");
+        }
+
+        private static void MostrarAyuda()
+        {
+            Console.WriteLine("Uso: param [-h] [-n <value>] [-a <value>]");
+            Console.WriteLine("\n Estos son los comando usados diferentes situaciones");
+            Console.WriteLine("\n Agregar informacion");
+            Console.WriteLine("-n         Nombre del usuario");
+            Console.WriteLine("-a         Apellido del usuario");
+        }
+    }
+}
